e9f4be64b60f9c62adc47ca56bc6aa3a72f3ae47
A l - A l a m y <59588007+AL-AlamySploit@users.noreply.github.com>
2021-05-29 00:11:28 +0200

    Add files via upload

---
+++ file/banner/toolkit.py
@@ -0,0 +1,11 @@
+#set color
+
+print "                                 88 88        88          
+print "    ,d                           88 88        ""   ,d     
+print "    88                           88 88             88     
+print "  MM88MMM ,adPPYba,   ,adPPYba,  88 88   ,d8  88 MM88MMM  
+print "    88   a8"     "8a a8"     "8a 88 88 ,a8"   88   88     
+print "    88   8b       d8 8b       d8 88 8888[     88   88     
+print "    88,  "8a,   ,a8" "8a,   ,a8" 88 88`"Yba,  88   88,    
+print "    "Y888 `"YbbdP"'   `"YbbdP"'  88 88   `Y8a 88   "Y888  
+                                                        
---
+++ file/banner/toolkit3.py
@@ -0,0 +1,10 @@
+# Set Color
+L = '\033[1;36m' #L
+print ""+L+""
+print " ____________"
+print "< tookit >"
+print " ------------"
+print "       \   ,__,"
+print "        \  (oo)____"
+print "           (__)    )\ "
+print "              ||--|| *"
