d76ad2f887dad068cb356c4ae5289ce9289848ab
A l - A l a m y <59588007+AL-AlamySploit@users.noreply.github.com>
2021-06-01 23:28:13 +0200

    Update toolkit.py

--- file/banner/toolkit.py
+++ file/banner/toolkit.py
@@ -9,3 +9,4 @@ print "    88   a8"     "8a a8"     "8a 88 88 ,a8"   88   88     "
 print "    88   8b       d8 8b       d8 88 8888[     88   88     "
 print "    88,  "8a,   ,a8" "8a,   ,a8" 88 88`"Yba,  88   88,    "
 print "    "Y888 `"YbbdP"'   `"YbbdP"'  88 88   `Y8a 88   "Y888  "                                                        
+banner()
